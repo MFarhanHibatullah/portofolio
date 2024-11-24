40d4c5f34f6654c175096bb79df1a6ed743f2063
MFarhanHibatullah <rrupecunkoko@gmail.com>
2024-11-24 08:14:25 -0800

    Memperbarui index.html dengan komentar tambahan

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@ window.addEventListener('scroll', function () {
         nav.classList.remove('nav-light');
     }
 })
+
+var icon = document.getElementById("mode");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+
+    });
+
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
